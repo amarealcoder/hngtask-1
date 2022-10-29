1598a0c032ee5378ad934f444ff2e679bc57cf9f
amarealcoder <ugorjimiracle06@gmail.com>
2022-10-29 12:24:23 +0100

    Fix header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,14 @@
+import './header.css';
 import image from './profile__img.png';
+
 const Header = () => {
   return (
     <section>
-      <div>
-        <div className='container__img'>
-          <img src={image} alt='profile image' id='profile__img' />
-        </div>
-        <h1>Miracle Ugorji</h1>
-        <h2 id='slack'>Miracle</h2>
+      <div className='container__img'>
+        <img src={image} alt='profile image' id='profile__img' />
       </div>
+      <h1 className='name'>Miracle Ugorji</h1>
+      <h2 className='hidden__name' id='slack'>Miracle</h2>
     </section>
   );
 };
