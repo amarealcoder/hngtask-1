7976ca7a2819bd422015847998afe215cb8a0ea2
amarealcoder <ugorjimiracle06@gmail.com>
2022-11-04 18:37:13 +0100

    Feature: Contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,3 +1,31 @@
 export const Contact = () => {
-    return <h1>Hello contact</h1>
-}
+  return (
+    <section>
+      <h1>Contact me</h1>
+      <h2>Hi, there. Contact me to ask me about anything you have in mind</h2>
+      
+      <form action='/action_page.php'>
+        <label for='fname'>First name:</label>
+        <input type='text' id='first_name' value='Enter your first name' />
+
+        <label for='lname'>Last name:</label>
+        <input type='text' id='last_name' value='Enter your last name' />
+
+        <label for='email'>Email</label>
+        <input type='email' id='email' value='yourname@email.com' />
+
+        <label for='textArea'>Message</label>
+        <textarea
+          id='message'
+          value='Send me a message and  reply as soon as I can '
+        ></textarea>
+
+        <input type='radio' />
+        <label>
+          You agree to providing your data to Ada who may contact you?
+        </label>
+        <button id='btn__submit'>Send message</button>
+      </form>
+    </section>
+  );
+};
