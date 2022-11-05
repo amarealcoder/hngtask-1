f03aa0bb5996a3e05b4baf7f1514c6e9324a2b5f
amarealcoder <ugorjimiracle06@gmail.com>
2022-11-05 07:27:06 +0100

    Style: Changes some contact styles

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,29 +1,42 @@
+import './contact.css';
+
 export const Contact = () => {
   return (
-    <section>
-      <h1>Contact me</h1>
-      <h2>Hi, there. Contact me to ask me about anything you have in mind</h2>
-      
+    <section className='section'>
+      <h3>Contact me</h3>
+      <p>Hi, there. Contact me to ask me about anything you have in mind.</p>
+
       <form action='/action_page.php'>
-        <label for='fname'>First name:</label>
-        <input type='text' id='first_name' value='Enter your first name' />
+        <div className='name-group'>
+          <div className='separate'>
+            <label for='fname'>First name</label>
+            <input type='text' id='first_name' value='Enter your first name' />
+          </div>
 
-        <label for='lname'>Last name:</label>
-        <input type='text' id='last_name' value='Enter your last name' />
+          <div className='separate'>
+            <label for='lname'>Last name</label>
+            <input type='text' id='last_name' value='Enter your last name' />
+          </div>
+        </div> 
 
-        <label for='email'>Email</label>
-        <input type='email' id='email' value='yourname@email.com' />
+        <div className='separate'>
+          <label for='email'>Email</label>
+          <input type='email' id='email' value='yourname@email.com' />
+        </div>
 
-        <label for='textArea'>Message</label>
-        <textarea
-          id='message'
-          value='Send me a message and  reply as soon as I can '
-        ></textarea>
+        <div className='separate'>
+          <label for='textArea'>Message</label>
+          <textarea id='message'>
+            Send me a message and reply as soon as I can
+          </textarea>
+        </div>
 
-        <input type='radio' />
-        <label>
-          You agree to providing your data to Ada who may contact you?
-        </label>
+        <div className='separate checkbox-container'>
+          <input type='checkbox' />
+          <label className=' checkbox-text'>
+            You agree to providing your data to Ada who may contact you?
+          </label>
+        </div>
         <button id='btn__submit'>Send message</button>
       </form>
     </section>
